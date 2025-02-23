9b44cce1d62b8f1eaca92f1ed6ca87c2964ecdef
Shriya Dale <shriyadale85@gmail.com>
2025-02-22 20:03:06 -0500

    fixed merge

--- App.tsx
+++ App.tsx
@@ -7,8 +7,6 @@ import LoginScreen from './screens/LoginScreen';
 import IntentScreen from './screens/IntentScreen';
 import ProfileFormScreen from './screens/ProfileFormScreen';
 import SwipeScreen from './screens/SwipeScreen';
-import ViewScreen from './screens/ViewScreen';
-
 
 // Define the type for route parameters
 export type RootStackParamList = {
@@ -16,7 +14,6 @@ export type RootStackParamList = {
   Intent: undefined;
   ProfileForm: { type: 'friend' | 'partner' };
   Swipe: undefined;
-  View: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -43,10 +40,6 @@ export default function App() {
           <Stack.Screen 
             name="Swipe" 
             component={SwipeScreen}
-          />
-          <Stack.Screen 
-            name="View" 
-            component={ViewScreen}
           />
         </Stack.Navigator>
       </NavigationContainer>
